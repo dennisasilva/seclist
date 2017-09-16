0f39bde0ee489a03de2f8a9c2118f178dd1b246e
Dennis Silva <dennis.a.silva@gmail.com>
2017-09-16 10:53:18 -0300

    Update seclist.sh

--- seclist.sh
+++ seclist.sh
@@ -12,26 +12,13 @@
 echo "\nScript para fazer o download das informações da lista full-disclosure no seclist.ogr"
 
 #message year
-echo "\n\nPor favor insira o ano [YYYY]:"
-echo "[2003] - [2004] - [2005] - [2006] - [2007] - [2008] - [2009] -[2010] - [2011] - [2012] - [2013] - [2014] - [2015] - [2016] - [2017]"
+echo "\n\nPor favor insira o ano [YYYY]: Ex: 2017"
 
 #variable year
 read year
 
 #message and month
-echo "\nPor favor insira o mês [MM]:"
-echo "[01 - JAN] - "
-echo "[02 - FEV]"
-echo "[03 - MAR]"
-echo "[04 - ABR]"
-echo "[05 - MAI]"
-echo "[06 - JUN"
-echo "[07 - JUL]"
-echo "[08 - AGO]"
-echo "[09 - SET]"
-echo "[10 - OUT]"
-echo "[11 - NOV]"
-echo "[12 - DEZ]"
+echo "\nPor favor insira o mês [MM]: Ex: 01"
 
 #variable month
 read month
@@ -59,5 +46,11 @@ esac
 #download wegpage
 wget seclist.org/fulldisclosure/$year/$month/index.html --output-document=/tmp/seclist-$month-$year-draft.txt
 
-#OkDok
+#tratative
 cat /tmp/seclist-$month-$year-draft.txt | egrep '<a name=' | awk -F ">" '{print $3}' | awk -F "<" '{print $1}' > /tmp/seclist-$month-$year.txt
+
+#remove DRAFT file
+rm -rf /tmp/seclist-$month-$year-draft.txt
+
+#output
+echo "Script finalizado com sucesso!!! Confira o arquivo em /tmp/seclist-$month-$year.txt"
